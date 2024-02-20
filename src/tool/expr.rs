e4807408159c71a69c6f16fcdb4575d5a7e49485
bignut <1214002133@qq.com>
2024-02-20 16:52:47 +0800

    chapter 5 finished.

--- src/tool/expr.rs
+++ src/tool/expr.rs
@@ -34,13 +34,16 @@ impl Expr {
                 operator,
                 right,
             } => {
-                todo!()
+                let left_str = (*left).two_string();
+                let operator_str = operator.lexeme.clone();
+                let right_str = (*right).two_string();
+                format!("( {} {} {} )", operator_str, left_str, right_str)
             }
             Expr::Grouping { expression } => {
-                todo!()
+                format!("( {} )", (*expression).two_string())
             }
             Expr::Literal { value } => {
-                todo!()
+                format!("{}", value.two_string())
             }
         }
     }
@@ -50,11 +53,23 @@ impl Expr {
 }
 
 pub enum ExprLiteral {
-    Number,
-    String,
+    NumberLiteral(f64),
+    StringLiteral(String),
     True,
     False,
     Nil,
+}
+
+impl ExprLiteral {
+    fn two_string(&self) -> String {
+        match self {
+            Self::NumberLiteral(v) => v.to_string(),
+            Self::StringLiteral(v) => v.clone(),
+            Self::True => "True".to_string(),
+            Self::False => "False".to_string(),
+            Self::Nil => "Nil".to_string(),
+        }
+    }
 }
 
 // pub struct BinaryExpr {
@@ -62,3 +77,35 @@ pub enum ExprLiteral {
 //     operator: Token,
 //     right: Box<Expr>,
 // }
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::Binary;
+
+    use crate::lexer::token::{LiterialValue, Token, TokenType};
+    use crate::tool::expr::{Expr, ExprLiteral};
+
+    #[test]
+    fn test_expr() {
+        let left1 = Box::new(Expr::Unary {
+            operator: Token::new(TokenType::Minus, "-".to_string(), None, 1),
+            right: Box::new(Expr::Literal {
+                value: ExprLiteral::NumberLiteral(239.0),
+            }),
+        });
+        let right1 = Box::new(Expr::Grouping {
+            expression: Box::new(Expr::Literal {
+                value: ExprLiteral::NumberLiteral(45.67),
+            }),
+        });
+
+        let test = Expr::Binary {
+            left: left1,
+            operator: Token::new(TokenType::Star, "*".to_string(), None, 1),
+            right: right1,
+        };
+
+        test.print();
+    }
+    // cargo test expr -- --nocapture
+}
